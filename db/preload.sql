dbe8986d6b579b83a81cd117895e7920481f0e0c
Tom Cammann <cammann.tom@gmail.com>
2014-08-23 18:45:03 +0000

    move to jinja, add more test data

--- db/preload.sql
+++ db/preload.sql
@@ -1,6 +1,9 @@
 INSERT INTO player(name) VALUES ('Tom');
 INSERT INTO player(name) VALUES ('Matt');
+INSERT INTO player(name) VALUES ('Mark');
+INSERT INTO player(name) VALUES ('Sam');
 
+/* Tom wins 16 games against matt */
 INSERT INTO game (winner, opponent, gamedate) VALUES
     (1, 2, date('now')),
     (1, 2, date('now')),
@@ -19,6 +22,7 @@ INSERT INTO game (winner, opponent, gamedate) VALUES
     (1, 2, date('now')),
     (1, 2, date('now'));
 
+/* Matt wins 14 games against Tom */
 INSERT INTO game (winner, opponent, gamedate) VALUES
     (2, 1, date('now')),
     (2, 1, date('now')),
@@ -34,3 +38,16 @@ INSERT INTO game (winner, opponent, gamedate) VALUES
     (2, 1, date('now')),
     (2, 1, date('now')),
     (2, 1, date('now'));
+
+/* Tom wins 10 games against Sam */
+INSERT INTO game (winner, opponent, gamedate) VALUES
+    (1, 3, date('now')),
+    (1, 3, date('now')),
+    (1, 3, date('now')),
+    (1, 3, date('now')),
+    (1, 3, date('now')),
+    (1, 3, date('now')),
+    (1, 3, date('now')),
+    (1, 3, date('now')),
+    (1, 3, date('now')),
+    (1, 3, date('now'));
--- web/webserver.py
+++ web/webserver.py
@@ -2,8 +2,9 @@
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 from collections import namedtuple
 import sqlite3
-import pystache
+import jinja2
 import os
+import re
 
 PORT_NUMBER = 8080
 
@@ -30,12 +31,12 @@ class FoosDatabase(object):
 
     def score(self, player_one, player_two):
             cursor = conn.cursor()
-            score_query = ('SELECT COUNT(id) FROM game WHERE '
-                           'winner = {player_one.id} AND opponent = {player_two.id}')
-
-            cursor.execute(score_query.format(player1=players[0], player2=players[1]))
+            score_query = jinja2.Template('SELECT COUNT(id) FROM game WHERE '
+                                          'winner = {{player_one.id}} AND opponent = {{player_two.id}}')
+           
+            cursor.execute(score_query.render(player_one=player_one, player_two=player_two))
             score_one = cursor.fetchone()[0]
-            cursor.execute(score_query.format(player1=players[1], player2=players[0]))
+            cursor.execute(score_query.render(player_one=player_two, player_two=player_one))
             return (score_one, cursor.fetchone()[0])
 
     def player(self, id):
@@ -43,14 +44,30 @@ class FoosDatabase(object):
         player_query = 'SELECT name FROM player WHERE id = {}'.format(id)
         cursor.execute(player_query)
         return { 'name': cursor.fetchone()[0], 'id': id }
+    
+    def opponents(self, player):
+        cursor = conn.cursor()
+        opponent_query = jinja2.Template('select distinct opponent from (select winner, opponent '
+                          'from game where winner = {{player.id}} union all '
+                          'select opponent, winner from game where opponent = {{player.id}})')
+        # return player id
+        cursor.execute(opponent_query.render(player=player))
+        return [ self.player(i[0]) for i in cursor.fetchall() ]
+
+    def scores(self, player):
+        scores = []
+        for i in db.opponents(player):
+            player_score, opponent_score = db.score(player, i)
+            scores.append({'opponent':i, 'opponent_score':opponent_score, 'player_score':player_score})
+        return scores
 
 db = FoosDatabase('foosball.db')
 
 class MyHandler(BaseHTTPRequestHandler):
         
-    def render_template(self, template_name, *args, **kwargs):
-        data = open('web/templates/' + template_name, 'r').read()
-        return pystache.render(data, *args, **kwargs)
+    def render_template(self, template_name, **kwargs):
+        data = jinja2.Template(open('web/templates/' + template_name, 'r').read())
+        return data.render(**kwargs)
 
     # Handler for the GET requests
     def do_GET(self):
@@ -60,12 +77,15 @@ class MyHandler(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(self.render_template('home.html', players=db.players(), title='FOOS'))
 
-        elif self.path.startswith('/player/'):
+        # elif self.path.startswith('/player/'):
+        elif re.match(r"/player/(\d+)/?", self.path):
+            id = int(re.match(r"/player/(\d+)/?", self.path).group(1))
             self.send_response(200)
             self.send_header('Content-type','text/html')
             self.end_headers()
-            scores = { 'opponent': {'name': 'Matt'}, 'opponent_score': 1, 'player_score': 2}
-            self.wfile.write(self.render_template('player.html', player=db.player(1), scores=scores,title='FOOS'))
+            player = db.player(id)
+            scores = db.scores(player)
+            self.wfile.write(self.render_template('player.html', player=player, scores=scores,title='FOOS'))
             
         else:
             self.send_error(404,'File Not Found: %s' % self.path)
